1df6a61fb4d3958c65bbd8317e6008f8d153a8ba
Leonard Lesinski <84378319+Le0X8@users.noreply.github.com>
2024-11-03 17:01:36 +0100

    Neue Links

--- src/routes/list.php/+server.ts
+++ src/routes/list.php/+server.ts
@@ -1,5 +1,5 @@
 import { redirect } from '@sveltejs/kit';
 
 export function GET() {
-  return redirect(301, 'https://2024.donald.org/forum');
+  return redirect(301, 'https://donald.org/forum');
 }
--- src/routes/read.php/+server.ts
+++ src/routes/read.php/+server.ts
@@ -2,5 +2,5 @@ import { redirect } from '@sveltejs/kit';
 import { numToBase64Url } from '$lib';
 
 export function GET({ url }) {
-  return redirect(301, 'https://2024.donald.org/forum/thread/' + numToBase64Url(parseInt(url.search.split(',')[1]) - 1));
+  return redirect(301, 'https://donald.org/forum/thread/' + numToBase64Url(parseInt(url.search.split(',')[1]) - 1));
 }
